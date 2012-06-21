a25f3b2a14bd54c288944d0760719fd418d8cf3a
nybon <niyue.com@gmail.com>
2012-06-21 16:00:00 +0800

    Create a skeleton for the server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tigol::Application.config.secret_token = 'b2222e886cff533ba26a3de96036f55eefd169444bcaecdfad2c6bd5aab29dbc61d496a73a382b3da5eafed231f21fb35a242844bf5a77ab8028fdaf25b73a7e'
